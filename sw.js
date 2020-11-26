80ffeb97339341918ab426067a099b2743906a3a
nicolas-van <nicolas-van@users.noreply.github.com>
2020-11-26 20:28:29 +0000

    deploy: 36d26461e0d8ccf1295d8f441f063af521297c92

---
+++ sw.js
@@ -0,0 +1,50 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "google-fonts-tester.a5930bd2.js",
+    "revision": "693cdb1106664f07e87b178879ea795b"
+  },
+  {
+    "url": "index.html",
+    "revision": "7198a92e33165c217941c0178a8b11d4"
+  },
+  {
+    "url": "logo192.eae72d5d.png",
+    "revision": "581fa1d82b7152e685510b51d48edd3e"
+  },
+  {
+    "url": "logo512.53123a4f.png",
+    "revision": "260d57586012b0ed1ae78accc0bf7083"
+  },
+  {
+    "url": "/",
+    "revision": "75beafbfcd494a18bf8de5b0fe6ec295"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL(".//index.html"));
